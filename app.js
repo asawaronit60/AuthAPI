df172fa1166003d8639aac8aa3e0419cbadd4a0c
Ronit <70330163+asawaronit60@users.noreply.github.com>
2021-06-20 00:55:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const path = require('path')
+const dotenv = require('dotenv')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+const viewRoutes = require('./Routes/viewRoute')
+const userRoutes = require('./Routes/userRoute');
+const PORT = process.env.PORT || 5000
+
+dotenv.config({path: './config.env'})
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+
+const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DB_PASSWORD
+);
+
+mongoose.connect(DB ,{useNewUrlParser: true, useUnifiedTopology: true})
+      .then(console.log("DB connection is successfull"))
+mongoose.set("useCreateIndex" , true)
+
+app.set('view engine','pug')
+app.set('views' , path.join(__dirname,'views'))
+
+/////////ROUTES/////////
+app.use(passport.initialize())
+
+require('./config/passport')(passport)
+
+app.use('/' , viewRoutes)
+app.use('/user',userRoutes)
+
+app.use((err,req,res)=>{
+    res.status(500).json({
+        error:err.message
+    })
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server is runnig at port 5000")
+})
+
